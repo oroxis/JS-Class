e45accffa798b356e954e06bba1f285af86ea336
oroxis <85342801+oroxis@users.noreply.github.com>
2021-07-02 11:54:41 +0100

    Add files via upload

---
+++ class.js
@@ -0,0 +1,36 @@
+
+// const number1 = 64;
+// const result1 = Math.sqrt(number1);
+// console.log(`The square root of ${number1} is ${result1}`);
+
+
+// const number2 = prompt('Please enter your number :'    )
+// const result2 = Math.sqrt(number2);
+// document.getElementById('num').innerHTML = `The square root of ${number2} is ${result2}`;
+
+
+// 1/2 * base * height
+const base = prompt('Enter the base of the Triangle :' );
+const height = prompt('Enter the height of the Triangle :' );
+
+let Area = 1/2 * base * height;
+
+console.log(Area)
+
+document.getElementById('area').innerHTML = `The area of the triangle with base ${base} and height ${height} is ${Area}`
+
+
+    let length1 = 34;
+    let length2 = 50;
+    let length3 = 20;
+
+    
+
+
+let naira = 0
+let dollar = naira*493
+let pound = naira*710
+let euro = naira*595
+
+
+

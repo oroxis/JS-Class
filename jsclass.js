89fb7583148641a41487b6969e58866c27c2625b
oroxis <85342801+oroxis@users.noreply.github.com>
2021-07-09 10:37:40 +0100

    Add files via upload

--- jsclass.js
+++ jsclass.js
@@ -31,17 +31,40 @@
 // .then(function(data) {console.log(data);})
 // .catch(function(error){console.log(error);})
 
+// const uii = document.getElementById('candidates')
+// fetch('https://randomuser.me/api/?results=10')
+//     .then((resp) => resp.json())
+//     .then(function(data) {
+//         let users = data.results;
+//         return users.map((user) => {
+//             const li = document.createElement('li');
+//             const img = document.createElement('img');
+//             const p = document.createElement('p');
+
+//             candidates.appendChild(li)
+//     .catch(function(error)) {
+
+//     }      
+
+//         });
+const uii = document.getElementById('candidates')
 fetch('https://randomuser.me/api/?results=10')
-    .then((resp) => {
-        return resp.json();
-    }).then(data => {
-       console.log(data);
+    .then((resp) => resp.json())
+    .then(function(data) {
+        let users = data.results;
+        return users.map((user) => {
+            const li = document.createElement('li');
+            const img = document.createElement('img');
+            const p = document.createElement('p');
+
+            candidates.appendChild(li);
+            let paragraph = li.appendChild(p);
+            let image = li.appendChild(img);
+            let firstName = user.name.first;
+            let lastName = user.name.last;
+            console.log(user.picture.large);
+            paragraph.innerHTML = `${firstName} ${lastName}`;
+            image.src = user.picture.large;
+        });
     })
-    .catch(function(error){
-        console.log(error);
-    });
-
-    // let candidates = data.results
-    // // return candidates
-    
-
+            .catch((error) => console.log(error));
